536f21bd9d17c51a6e5f185ab360530af1db90a6
cragtok <lidfun@protonmail.com>
2024-10-20 20:10:25 +0600

    exercise 11.9

---
+++ e2e-tests/pokedex.spec.js
@@ -0,0 +1,15 @@
+import { test, describe, expect } from '@playwright/test'
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('http://localhost:8080/')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('individual Pokemon page can be opened', async ({ page }) => {
+    await page.goto('http://localhost:8080/pokemon/ivysaur')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
+})
