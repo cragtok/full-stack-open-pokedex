20dbbb02f973b3ddec9c56bd4cf786799d3b0269
cragtok <lidfun@protonmail.com>
2024-10-29 16:25:36 +0600

    exercise 11.10

--- app.mjs
+++ app.mjs
@@ -1,13 +1,17 @@
 import express from 'express'
 import process from 'node:process'
 
-const { static: expressStatic } = express;
+const { static: expressStatic } = express
 const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
 app.use(expressStatic('dist'))
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 
 app.listen(PORT, () => {
   /* eslint-disable */
